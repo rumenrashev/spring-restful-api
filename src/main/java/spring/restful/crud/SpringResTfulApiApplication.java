34b10cbdf4f6c994202a1f7fca5f453faa779f74
Rumen Rashev <57558028+rumenrashev@users.noreply.github.com>
2021-07-06 11:35:50 +0300

    create IntelliJIdea project

---
+++ src/main/java/spring/restful/crud/SpringResTfulApiApplication.java
@@ -0,0 +1,13 @@
+package spring.restful.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringResTfulApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringResTfulApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/spring/restful/crud/SpringResTfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.restful.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringResTfulApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
